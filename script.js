c7fa4bcf3528dacdda57bddf6af7e9c067f0c416
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2023-05-12 21:45:44 -0700

    Final
    Changed a lot of broken mechanics. The background now is in constant motion. This is the final update for the project.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+let clicks = 1;
 function navBTN() {
-    let mains = document.getElementsByClassName("mainDiv");
-    for (i = 0; i < mains.length; i++) {
-        mains[i].style.display = "none";
-        console.log(mains[i]);
+    if( clicks == 1){
+        clicks--;
+        let mains = document.getElementsByClassName("mainDiv");
+        for (i = 0; i < mains.length; i++) {
+            mains[i].style.display = "none";
+        }
+        console.log(clicks);
+    } else {
+        clicks++;
+        let mains = document.getElementsByClassName("mainDiv");
+        for (i = 0; i < mains.length; i++) {
+            mains[i].style.display = "flex";
+        }
+        console.log(clicks);
     }
 }
